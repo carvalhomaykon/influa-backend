050903fe40425935007f754d295f261056b2f853
carvalhomaykon <carvalhomaykon566@gmail.com>
2026-05-29 08:17:07 -0300

    first commit

---
+++ src/main/java/com/influa/influa/InfluaApplication.java
@@ -0,0 +1,13 @@
+package com.influa.influa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/influa/influa/InfluaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.influa.influa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
