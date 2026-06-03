42b9f4bc79e9ac1ad74c67f85b6be09ba589abd0
Maykon Carvalho <carvalhomaykon566@gmail.com>
2026-06-03 12:15:56 -0300

    feat(service): Creating user-related services

---
+++ src/main/java/com/influa/influa/services/user/InfluencerService.java
@@ -0,0 +1,33 @@
+package com.influa.influa.services.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.influa.influa.dtos.user.InfluencerDTO;
+import com.influa.influa.dtos.user.UserDTO;
+import com.influa.influa.model.user.Influencer;
+import com.influa.influa.repositories.user.InfluencerRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class InfluencerService {
+
+    @Autowired
+    private InfluencerRepository influencerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Influencer createInfluencer(UserDTO userDTO, InfluencerDTO influencerDTO) {
+
+        Influencer newInfluencer = new Influencer(userDTO, influencerDTO);
+
+        newInfluencer.setPassword(passwordEncoder.encode(userDTO.password()));
+        
+        return influencerRepository.save(newInfluencer);
+    }
+    
+}
