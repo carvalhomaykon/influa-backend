2a30d92bfababde304b57ef15776ca9e18e421af
Maykon Carvalho <carvalhomaykon566@gmail.com>
2026-06-03 12:16:43 -0300

    feat(repositories): Creating Social Network repository

---
+++ src/main/java/com/influa/influa/repositories/socialnetwork/SocialNetworkRepository.java
@@ -0,0 +1,8 @@
+package com.influa.influa.repositories.socialnetwork;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.influa.influa.model.socialnetwork.SocialNetwork;
+
+public interface SocialNetworkRepository extends JpaRepository<SocialNetwork, Long> {
+}
