a5214ef1765023933d80e795e0cc7ba441e4710a
Maykon Carvalho <carvalhomaykon566@gmail.com>
2026-06-03 12:14:17 -0300

    feat(model): add model Influencer rate

---
+++ src/main/java/com/influa/influa/model/influencerrate/InfluencerRate.java
@@ -0,0 +1,43 @@
+package com.influa.influa.model.influencerrate;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import com.influa.influa.model.socialnetwork.EnumPlatform;
+import com.influa.influa.model.user.Influencer;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "influencer_rate")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class InfluencerRate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private EnumPlatform platform;
+    private Integer amount;
+    private BigDecimal startingPrice;
+    private BigDecimal endPrice;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "influencer_id")
+    private Influencer influencer;
+
+}
