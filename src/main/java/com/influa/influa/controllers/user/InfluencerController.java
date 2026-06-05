f24dcfb6a38864ae31adb24822f4d84a623c3a3b
carvalhomaykon <carvalhomaykon566@gmail.com>
2026-06-05 18:05:18 -0300

    feat(Influencer): Completing the CRUD methods of the class

--- src/main/java/com/influa/influa/controllers/user/InfluencerController.java
+++ src/main/java/com/influa/influa/controllers/user/InfluencerController.java
@@ -4,26 +4,49 @@ import com.influa.influa.dtos.user.InfluencerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.influa.influa.model.user.Influencer;
 import com.influa.influa.services.user.InfluencerService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/users/influencers")
 public class InfluencerController {
-    
+
     @Autowired
     private InfluencerService influencerService;
 
-    @PostMapping("/influencer")
+    @PostMapping
     public ResponseEntity<Influencer> registerInfluencer(@RequestBody InfluencerDTO data) {
         Influencer influencer = this.influencerService.createInfluencer(data);
 
         return new ResponseEntity<>(influencer, HttpStatus.CREATED);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Influencer>> getAllInfluencers() {
+        List<Influencer> influencers = influencerService.findAllInfluencers();
+        return new ResponseEntity<>(influencers, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Influencer> getInfluencerById(@PathVariable UUID id) {
+        Influencer influencer = influencerService.findInfluencerById(id);
+        return new ResponseEntity<>(influencer, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Influencer> updateInfluencer(@PathVariable UUID id, @RequestBody InfluencerDTO influencerDTO) {
+        Influencer updateInfluencer = influencerService.updateInfluencer(id, influencerDTO);
+        return new ResponseEntity<>(updateInfluencer, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteInfluencer(@PathVariable UUID id) {
+        influencerService.deleteInfluencer(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/influa/influa/services/user/InfluencerService.java
+++ src/main/java/com/influa/influa/services/user/InfluencerService.java
@@ -5,11 +5,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.influa.influa.dtos.user.InfluencerDTO;
-import com.influa.influa.dtos.user.UserDTO;
 import com.influa.influa.model.user.Influencer;
 import com.influa.influa.repositories.user.InfluencerRepository;
 
-import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class InfluencerService {
@@ -20,14 +20,37 @@ public class InfluencerService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Transactional
     public Influencer createInfluencer(InfluencerDTO influencerDTO) {
-
         Influencer newInfluencer = new Influencer(influencerDTO);
 
         newInfluencer.setPassword(passwordEncoder.encode(influencerDTO.userDTO().password()));
         
         return influencerRepository.save(newInfluencer);
+    }
+
+    public Influencer findInfluencerById(UUID id) {
+        return influencerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Influencer não encontrado com o ID: " + id));
+    }
+
+    public List<Influencer> findAllInfluencers() {
+        return influencerRepository.findAll();
+    }
+
+    public Influencer updateInfluencer(UUID id, InfluencerDTO influencerDTO) {
+        Influencer influencer = influencerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Contratante não encontrado com o ID: " + id));
+
+        influencer.setName(influencerDTO.userDTO().name());
+        influencer.setEmail(influencerDTO.userDTO().email());
+        influencer.setPassword(influencerDTO.userDTO().password());
+
+        return influencerRepository.save(influencer);
+    }
+
+    public void deleteInfluencer(UUID id) {
+        Influencer influencer = findInfluencerById(id);
+        influencerRepository.delete(influencer);
     }
     
 }
